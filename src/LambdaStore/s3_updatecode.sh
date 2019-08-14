5c6c5a466bfae8f857cda0053e80bed5ec3ef8c3
Tianium <zhangjyr@gmail.com>
2019-08-14 18:35:56 -0400

    Revert "Merge commit '9d2232af3f091d40bcbff5891fde90dda6c1e21f' into config/tianium"
    This reverts commit 02072238878c5096d499a56a33eee27ae571a542.

--- src/LambdaStore/s3_updatecode.sh
+++ src/LambdaStore/s3_updatecode.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-PREFIX="Store1VPCNode"
+PREFIX="Proxy1Node"
 if [ "$1" != "" ] ; then
   PREFIX="$1"
 fi
-mem=512
+# concurrency=30
 
 echo "compiling lambda code..."
 GOOS=linux go build redeo_lambda.go
 echo "compress file..."
-zip LambdaStore redeo_lambda
+zip Lambda2SmallJPG redeo_lambda
 echo "updating lambda code.."
 
 echo "putting code zip to s3"
-aws s3api put-object --bucket tianium.default --key lambdastore.zip --body LambdaStore.zip
+aws s3api put-object --bucket ao.lambda.code --key lambdastore.zip --body Lambda2SmallJPG.zip
 
 for i in {0..63}
 do
-     aws lambda update-function-code --function-name $PREFIX$i --s3-bucket tianium.default --s3-key lambdastore.zip
-     aws lambda update-function-configuration --function-name $PREFIX$i --memory-size $mem --timeout $2
+     aws lambda update-function-code --function-name $PREFIX$i --s3-bucket ao.lambda.code --s3-key lambdastore.zip
+     # aws lambda update-function-configuration --function-name $PREFIX$i --memory-size $mem
      # aws lambda update-function-configuration --function-name $PREFIX$i --timeout $2
 #    aws lambda update-function-configuration --function-name $PREFIX$i --handler redeo_lambda
 #    aws lambda put-function-concurrency --function-name $PREFIX$i --reserved-concurrent-executions $concurrency
