e547b111214f105280636cf24d79e35da010dafc
Tianium <zhangjyr@gmail.com>
2019-09-06 11:04:24 -0400

    update logDel

--- src/LambdaStore/logDel.sh
+++ src/LambdaStore/logDel.sh
@@ -1,9 +1,9 @@
 # deleting log in cloudWatch with prefix
 #!/bin/bash
 
-PREFIX=$1
+PREFIX="Store1VPCNode"
 
-for i in {0..64}
+for i in {0..299}
 do
     aws logs delete-log-group --log-group-name /aws/lambda/$PREFIX$i
 done
