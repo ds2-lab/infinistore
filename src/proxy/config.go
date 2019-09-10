2f9deb6a6889e5c8adf48b4b721ff4f613a874f4
Tianium <zhangjyr@gmail.com>
2019-09-09 21:17:50 -0400

    use 300

--- src/proxy/config.go
+++ src/proxy/config.go
@@ -6,8 +6,8 @@ import (
 	"github.com/wangaoone/LambdaObjectstore/src/proxy/lambdastore"
 )
 
-const LambdaMaxDeployments = 400
-const NumLambdaClusters = 400
+const LambdaMaxDeployments = 300
+const NumLambdaClusters = 300
 const LambdaStoreName = "LambdaStore"
 const LambdaPrefix = "Store1VPCNode"
 const InstanceWarmTimout = 1 * time.Minute
