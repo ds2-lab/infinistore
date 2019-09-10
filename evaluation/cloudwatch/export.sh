e18a69ab4bfe24ad0e6ba6b963329ad2d7e061c1
Tianium <zhangjyr@gmail.com>
2019-09-10 09:51:15 -0400

    Adjust config

--- evaluation/cloudwatch/export.sh
+++ evaluation/cloudwatch/export.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-LAMBDA="/aws/lambda/reclaim"
-FILE="aws/lambda/reclaim"
+LAMBDA="/aws/lambda/Store1VPCNode"
+FILE="/lambda/"
 
-start='2019-09-06 00:00:00'
-end='2019-09-09 00:00:00'
+PREFIX=$1
+start=$2
+end=$3
 
 # Convert date into seconds (Format is %s)
 startTime=$(date  -j -f "%Y-%m-%d %H:%M:%S" "$start" +%s)000
 endTime=$(date  -j -f "%Y-%m-%d %H:%M:%S" "$end" +%s)000
 
 
-for i in {0..5}
+for i in {0..399}
 do
-    aws logs create-export-task --log-group-name $LAMBDA$i --from ${startTime} --to ${endTime} --destination "ao.cost.log" --destination-prefix $FILE$i
-    sleep 2s
+    aws logs create-export-task --log-group-name $LAMBDA$i --from ${startTime} --to ${endTime} --destination "tianium.default" --destination-prefix $FILE$PREFIX$i
+    sleep 1s
 done
