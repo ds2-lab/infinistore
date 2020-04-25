1577358f245df0d0d1dcde2f008bc3700c8cd0ad
Tianium <zhangjyr@gmail.com>
2020-04-24 23:55:52 -0400

    Bug fix: returned request will not be abandoned.

--- proxy/global/requests.go
+++ proxy/global/requests.go
@@ -3,6 +3,7 @@ package global
 import (
 	"github.com/cornelk/hashmap"
 	"sync"
+	"sync/atomic"
 
 	"github.com/mason-leap-lab/infinicache/proxy/types"
 )
@@ -19,26 +20,22 @@ type RequestCoordinator struct {
 func NewRequestCoordinator(size uintptr) *RequestCoordinator {
 	return &RequestCoordinator{
 		pool: &sync.Pool{
-			New: newReqCounter,
+			New: newRequestCounter,
 		},
 		registry: hashmap.New(size),
 	}
 }
 
-func newReqCounter() interface{} {
-	return &types.ClientReqCounter{}
-}
-
-func (c *RequestCoordinator) Register(reqId string, cmd string, d int64, p int64) *types.ClientReqCounter {
-	prepared := c.pool.Get().(*types.ClientReqCounter)
+func (c *RequestCoordinator) Register(reqId string, cmd string, d int64, p int64) *RequestCounter {
+	prepared := c.pool.Get().(*RequestCounter)
 	ret, ok := c.registry.GetOrInsert(reqId, prepared)
-	counter := ret.(*types.ClientReqCounter)
+	counter := ret.(*RequestCounter)
 	if !ok {
 		// New counter registered, initialize values.
 		counter.Cmd = cmd
 		counter.DataShards = d
 		counter.ParityShards = p
-		counter.Returned = 0
+		counter.returned = 0
 		l := int(d + p)
 		if cap(counter.Requests) < l {
 			counter.Requests = make([]*types.Request, l)
@@ -55,15 +52,53 @@ func (c *RequestCoordinator) Register(reqId string, cmd string, d int64, p int64
 	return counter
 }
 
-func (c *RequestCoordinator) Load(reqId string) (*types.ClientReqCounter, bool) {
+func (c *RequestCoordinator) Load(reqId string) (*RequestCounter, bool) {
 	if counter, ok := c.registry.Get(reqId); ok {
-		return counter.(*types.ClientReqCounter), ok
+		return counter.(*RequestCounter), ok
 	} else {
 		return nil, ok
 	}
 }
 
-func (c *RequestCoordinator) Clear(reqId string, counter *types.ClientReqCounter) {
+func (c *RequestCoordinator) Clear(reqId string, counter *RequestCounter) {
 	c.registry.Del(reqId)
 	c.pool.Put(counter)
+}
+
+// Counter for returned requests.
+type RequestCounter struct {
+	Cmd          string
+	DataShards   int64
+	ParityShards int64
+	Requests     []*types.Request
+
+	returned     int64       // Returned counter from lambda.
+}
+
+func newRequestCounter() interface{} {
+	return &RequestCounter{}
+}
+
+func (c *RequestCounter) AddReturned(chunk int) int64 {
+	returned := atomic.AddInt64(&c.returned, 1)
+	if c.Requests[chunk] != nil {
+		c.Requests[chunk].MarkReturned()
+	}
+	return returned
+}
+
+func (c *RequestCounter) Returned() int64 {
+	return atomic.LoadInt64(&c.returned)
+}
+
+func (c *RequestCounter) IsFulfilled(returned int64) bool {
+	return returned >= c.DataShards
+}
+
+func (c *RequestCounter) IsLate(returned int64) bool {
+	return returned > c.DataShards
+}
+
+func (c *RequestCounter) IsAllReturned(returned int64) bool {
+	return returned >= c.DataShards + c.ParityShards
 }
