5113408b733c2c8be14be6c3b0bf275248ab5602
Tianium <zhangjyr@gmail.com>
2020-05-07 16:14:57 -0400

    Bug fix

--- proxy/global/requests.go
+++ proxy/global/requests.go
@@ -12,8 +12,8 @@ import (
 const (
 	REQCNT_STATUS_SUCCEED  = 0x0000000100000000
 	REQCNT_STATUS_RETURNED = 0x0000000000000001
-	REQCNT_MASK_SUCCEED    = 0x1111111100000000
-	REQCNT_MASK_RETURNED   = 0x0000000011111111
+	REQCNT_MASK_SUCCEED    = 0xFFFFFFFF00000000
+	REQCNT_MASK_RETURNED   = 0x00000000FFFFFFFF
 )
 
 var (
@@ -106,7 +106,7 @@ type RequestCounter struct {
 
 	coordinator *RequestCoordinator
 	reqId       string
-	status      int64 // int32(succeed) + int32(returned)
+	status      uint64 // int32(succeed) + int32(returned)
 }
 
 func newRequestCounter() interface{} {
@@ -117,60 +117,60 @@ func (c *RequestCounter) String() string {
 	return c.reqId
 }
 
-func (c *RequestCounter) AddSucceeded(chunk int) int64 {
-	status := atomic.AddInt64(&c.status, REQCNT_STATUS_SUCCEED+REQCNT_STATUS_RETURNED)
+func (c *RequestCounter) AddSucceeded(chunk int) uint64 {
+	status := atomic.AddUint64(&c.status, REQCNT_STATUS_SUCCEED+REQCNT_STATUS_RETURNED)
 	if c.Requests[chunk] != nil {
 		c.Requests[chunk].MarkReturned()
 	}
 	return status
 }
 
-func (c *RequestCounter) AddReturned(chunk int) int64 {
-	status := atomic.AddInt64(&c.status, REQCNT_STATUS_RETURNED)
+func (c *RequestCounter) AddReturned(chunk int) uint64 {
+	status := atomic.AddUint64(&c.status, REQCNT_STATUS_RETURNED)
 	if c.Requests[chunk] != nil {
 		c.Requests[chunk].MarkReturned()
 	}
 	return status
 }
 
-func (c *RequestCounter) Status() int64 {
-	return atomic.LoadInt64(&c.status)
+func (c *RequestCounter) Status() uint64 {
+	return atomic.LoadUint64(&c.status)
 }
 
-func (c *RequestCounter) Succeeded() int64 {
-	return (atomic.LoadInt64(&c.status) & REQCNT_MASK_SUCCEED) >> 32
+func (c *RequestCounter) Succeeded() uint64 {
+	return (atomic.LoadUint64(&c.status) & REQCNT_MASK_SUCCEED) >> 32
 }
 
-func (c *RequestCounter) Returned() int64 {
-	return atomic.LoadInt64(&c.status) & REQCNT_MASK_RETURNED
+func (c *RequestCounter) Returned() uint64 {
+	return atomic.LoadUint64(&c.status) & REQCNT_MASK_RETURNED
 }
 
-func (c *RequestCounter) IsFulfilled(status ...int64) bool {
+func (c *RequestCounter) IsFulfilled(status ...uint64) bool {
 	if len(status) == 0 {
 		return c.IsFulfilled(c.Status())
 	}
-	return (status[0]&REQCNT_MASK_SUCCEED)>>32 >= c.DataShards
+	return (status[0]&REQCNT_MASK_SUCCEED) >> 32 >= uint64(c.DataShards)
 }
 
-func (c *RequestCounter) IsLate(status ...int64) bool {
+func (c *RequestCounter) IsLate(status ...uint64) bool {
 	if len(status) == 0 {
 		return c.IsLate(c.Status())
 	}
-	return (status[0]&REQCNT_MASK_SUCCEED)>>32 > c.DataShards
+	return (status[0]&REQCNT_MASK_SUCCEED) >> 32 > uint64(c.DataShards)
 }
 
-func (c *RequestCounter) IsAllReturned(status ...int64) bool {
+func (c *RequestCounter) IsAllReturned(status ...uint64) bool {
 	if len(status) == 0 {
 		return c.IsAllReturned(c.Status())
 	}
-	return status[0]&REQCNT_MASK_RETURNED >= c.DataShards+c.ParityShards
+	return status[0]&REQCNT_MASK_RETURNED >= uint64(c.DataShards+c.ParityShards)
 }
 
 func (c *RequestCounter) Release() {
 	c.coordinator.Clear(c)
 }
 
-func (c *RequestCounter) ReleaseIfAllReturned(status ...int64) {
+func (c *RequestCounter) ReleaseIfAllReturned(status ...uint64) {
 	if c.IsAllReturned(status...) {
 		c.Release()
 	}
