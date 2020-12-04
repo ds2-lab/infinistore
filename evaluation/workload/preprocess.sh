1be3cb5d660a4fa6bdc510a3c5a18ee091ac9e15
Tianium <zhangjyr@gmail.com>
2020-12-04 17:14:08 -0500

    update script

---
+++ evaluation/workload/preprocess.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PREFIX=$1
+if [ "$PREFIX" == "" ] ; then
+  echo "Manual: preprocess prefix"
+  exit
+fi
+
+BASE=`pwd`/`dirname $0`
+BIN=$BASE/../bin
+DATA=$BASE/../downloaded
+OUTPUT=$DATA/$PREFIX
+
+mkdir -p $OUTPUT
+
+cd $BASE/..
+
+# Unzip
+mkdir -p $DATA/proxy/$PREFIX
+tar -xzf $DATA/proxy/$PREFIX.tar.gz -C $DATA/proxy/$PREFIX/
+
+# Decode .clog file
+./infla.sh $PREFIX
+
+# Extract cluster data from proxy output
+cat $OUTPUT/simulate-400_proxy.csv | grep cluster, > $OUTPUT/cluster.csv
+cat $OUTPUT/simulate-400_proxy.csv | grep bucket, > $OUTPUT/bucket.csv
+
+# Extract billing info from cloudwatch log
+cloudwatch/parse.sh $DATA/log/$PREFIX
+cat $DATA/log/${PREFIX}_bill.csv | grep invocation > $OUTPUT/bill.csv
+make build-data
+$BIN/preprocess -o $OUTPUT/recovery.csv -processor workload $DATA/data/$PREFIX
