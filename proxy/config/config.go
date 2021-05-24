793af01d037cf569fdbe2a54eeba2e24d96b27b2
Tianium <zhangjyr@gmail.com>
2021-05-24 09:23:25 -0400

    Reconfig for SR

--- proxy/config/config.go
+++ proxy/config/config.go
@@ -36,10 +36,10 @@ const InstanceDegradeWarmTimeout = 5 * time.Minute
 
 // InstanceCapacity Capacity of deployed Lambda functions.
 // TODO: Detectable on invocation. Can be specified by option -funcap for now.
-const DefaultInstanceCapacity = 1024 * 1000000 // 1GB
+const DefaultInstanceCapacity = 1536 * 1000000 // 1GB
 
 // InstanceOverhead Memory reserved for running program on Lambda functions.
-const InstanceOverhead = 200 * 1000000 // 200MB
+const InstanceOverhead = 300 * 1000000 // 200MB
 
 // Threshold Scaling out avg instance size threshold
 const Threshold = 0.8 // Don't set beyond 0.8
