076090da0c61eb77d067db3f8a4c4f93e64821c6
Tianium <zhangjyr@gmail.com>
2021-06-13 23:21:24 -0400

    Adjust file structure.

--- reclaim_test/singleDeployment/s3_updatecode.sh
+++ reclaim_test/singleDeployment/s3_updatecode.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-PWD=`dirname $0`
-
+BASE=`pwd`/`dirname $0`
 PREFIX=$1
 ROLE=$2
 BUCKET=$3
@@ -9,7 +8,7 @@ KEY="reclaim"
 mem=3008
 
 echo "compiling lambda code..."
-GOOS=linux go build $KEY.go
+GOOS=linux go build -o $KEY ./main_$KEY
 echo "compress file..."
 zip $KEY $KEY
 echo "updating lambda code.."
@@ -17,8 +16,7 @@ echo "updating lambda code.."
 echo "putting code zip to s3"
 aws s3api put-object --bucket ao.lambda.code --key $KEY.zip --body $KEY.zip
 
-#go run $PWD/../../sbin/deploy_function.go -from=0 -to=300 -create=true -config=true -prefix=$PREFIX -vpc=true -key=$KEY -mem=$mem -timeout=$1
-./deploy_function -from=0 -to=300 -role=$ROLE -bucket=$BUCKET -prefix=$PREFIX \
+go run $BASE/../../deploy/deploy_function.go -from=0 -to=300 -role=$ROLE -bucket=$BUCKET -prefix=$PREFIX \
 -create=true -config=true -vpc=false -key=$KEY -mem=$mem -timeout=200
 
 go clean
