4d7229675159975bb6f13c94d2f6ba776f806308
Tianium <zhangjyr@gmail.com>
2022-04-30 15:04:21 -0400

    Reset reconciling request after lambda returned.

--- proxy/lambdastore/instance.go
+++ proxy/lambdastore/instance.go
@@ -1107,6 +1107,8 @@ func (ins *Instance) doTriggerLambda(opt *ValidateOption) error {
 		} else {
 			ins.log.Debug("Got staled instance lineage: %v", &outputStatus)
 		}
+		// Reset reconciling request after lambda returned.
+		ins.reconcilingWorker = 0
 	}
 	return nil
 }
