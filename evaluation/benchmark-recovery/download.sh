9e0253e4d79badf39dc3b3a9f149b0a58deeaa1a
Tianium <zhangjyr@gmail.com>
2022-05-02 19:34:16 -0400

    fix config

--- evaluation/benchmark-recovery/download.sh
+++ evaluation/benchmark-recovery/download.sh
@@ -6,4 +6,4 @@ BASE=`pwd`/`dirname $0`
 DATA=$BASE/../downloaded/data/$PREFIX
 mkdir -p $DATA
 
-aws s3 cp s3://mason-leap-lab.datapool/data/$PREFIX $DATA --recursive
+aws s3 cp s3://tianium.default/data/$PREFIX $DATA --recursive
