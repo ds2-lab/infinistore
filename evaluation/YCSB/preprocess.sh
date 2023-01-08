6bc649a0c3f77d8a967efb0bc1896986971775fd
Tianium <zhangjyr@gmail.com>
2023-01-08 16:36:22 -0500

    update script

--- evaluation/YCSB/preprocess.sh
+++ evaluation/YCSB/preprocess.sh
@@ -11,4 +11,6 @@ cd $BASE/..
 
 make build-data
 $BIN/preprocess -o $OUTPUT/ycsb.csv -processor ycsb -yaml $BASE/preprocess.yml $DATA/data_redis
+$BIN/preprocess -a -o $OUTPUT/ycsb.csv -processor ycsb -yaml $BASE/preprocess.yml $DATA/data_redisc
 $BIN/preprocess -a -o $OUTPUT/ycsb.csv -processor ycsb -yaml $BASE/preprocess.yml $DATA/data_sion
+$BIN/preprocess -a -o $OUTPUT/ycsb.csv -processor ycsb -yaml $BASE/preprocess.yml $DATA/data_anna
