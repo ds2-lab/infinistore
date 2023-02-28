eb7356bec994ed16c32156bfa3688edc51746ab5
Tianium <zhangjyr@gmail.com>
2023-02-27 21:47:46 -0500

    Bug fix.

--- lambda/config.go
+++ lambda/config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"regexp"
 	"time"
 )
 
@@ -11,18 +12,21 @@ const (
 )
 
 var (
-	// Bucket to store persistent data. Keep "%s" at the end of the bucket name.
-	S3_BACKUP_BUCKET string = "infinistore.backup%s"
+	// Bucket to store persistent data.
+	S3_BACKUP_BUCKET string = "[your COS bucket]"
 	// Bucket to store experiment data. No date will be stored if InputEvent.Prefix is not set.
-	S3_COLLECTOR_BUCKET string = "ds2-lab.datapool"
+	S3_COLLECTOR_BUCKET string = "[your data bucket]"
 
-	DRY_RUN = true
+	DRY_RUN = false
 )
 
 func init() {
 	// Overwrite if environment variables are set.
 	S3_BACKUP_BUCKET = GetenvIf(os.Getenv("S3_BACKUP_BUCKET"), S3_BACKUP_BUCKET)
 	S3_COLLECTOR_BUCKET = GetenvIf(os.Getenv("S3_COLLECTOR_BUCKET"), S3_COLLECTOR_BUCKET)
+
+	// Normalize backup bucket name with a pending %s.
+	S3_BACKUP_BUCKET = regexp.MustCompile(`^(.*?)(%s)?$`).ReplaceAllString(S3_BACKUP_BUCKET, "$1%s")
 }
 
 func GetenvIf(env string, def string) string {
